3857aee960a6a1ca3ee25d9988cf021b783355b6
hlo1234567 <40270452+hlo1234567@users.noreply.github.com>
2020-11-24 21:49:47 -0500

    Add files via upload

---
+++ Code/ballCheck.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballCheck : MonoBehaviour
+{
+	public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Trap") {
+            other.gameObject.SetActive(false);
+            Time.timeScale = 0;
+
+        }
+    }
+}
