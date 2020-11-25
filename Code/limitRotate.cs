19c63d4be4df7f861b73a4e2d1c2bfe86f709008
hlo1234567 <40270452+hlo1234567@users.noreply.github.com>
2020-11-24 21:49:20 -0500

    Add files via upload

---
+++ Code/limitRotate.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class limitRotate : MonoBehaviour {
+ 
+  public float rotateSpeed;
+   
+  public float minimumX;
+  public float maximumX;
+ 
+  public float minimumY;
+  public float maximumY;
+ 
+  float rotationX = 0F;
+  float rotationY = 0F;
+   
+   
+   // Use this for initialization
+  void Start ()
+  {
+   
+  }
+   
+  public static float ClampAngle (float angle, float min, float max)
+  {
+    if (angle < -360F) angle += 360F;
+    if (angle > 360F) angle -= 360F;
+     
+    return Mathf.Clamp (angle, min, max);
+       
+  }
+   
+ 
+  void Update ()
+  {
+     
+    rotationY += Input.GetAxis("Vertical") * rotateSpeed * Time.deltaTime;
+    rotationY = ClampAngle (rotationY, minimumY, maximumY);
+         
+    rotationX+= -Input.GetAxis("Horizontal") * rotateSpeed * Time.deltaTime;
+    rotationX = ClampAngle (rotationX, minimumX, maximumX);
+         
+                   
+    Quaternion yQuaternion = Quaternion.AngleAxis (rotationY, Vector3.right);
+    Quaternion xQuaternion = Quaternion.AngleAxis (rotationX, Vector3.forward);
+    transform.rotation=   yQuaternion * xQuaternion;
+ 
+   }
+}    
