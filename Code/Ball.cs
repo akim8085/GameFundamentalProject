d19853ba0e187867a2e5c76848fbe7fb36e3ea43
akim8085 <41929867+akim8085@users.noreply.github.com>
2020-11-25 20:59:23 -0500

    Add files via upload

---
+++ Code/Ball.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+ {
+     private Rigidbody rb;
+     void Start()
+     {
+         rb = GetComponent<Rigidbody>();
+     }
+ 
+     void Update()
+     {
+         if (rb.IsSleeping())
+         {
+             rb.WakeUp();
+         }
+     }
+ }
