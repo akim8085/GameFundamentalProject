59843483afec0a3671c5660a1f804a856dcba482
lornahokoth <60718374+lornahokoth@users.noreply.github.com>
2020-11-28 23:30:44 -0500

    Test from CMD; Same as ZIP

---
+++ BoardMaze/Script/BallController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public float ballSpeed;
+
+    void Start() {
+        AudioSource[] myAudio = this.GetComponents<AudioSource>();
+        myAudio[1].volume = 0;
+        myAudio[1].Play();
+    }
+   
+    // Update is called once per frame
+    void Update()
+    {
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+        Rigidbody body = GetComponent<Rigidbody>();
+        body.AddForce(new Vector3(xAxis, 0, yAxis));
+        AudioSource[] myAudio = this.GetComponents<AudioSource>();
+        myAudio[1].volume = GetComponent<Rigidbody>().velocity.magnitude / 20;
+    }
+}
---
+++ BoardMaze/Script/CollisionSound.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionSound : MonoBehaviour
+{
+    // volume 0 - 1
+    //magnatude 
+    void OnCollisionEnter(Collision other) {
+        if (other.gameObject.tag == "wall") {
+            AudioSource[] myAudio = this.GetComponents<AudioSource>();
+            myAudio[0].volume = other.relativeVelocity.magnitude / 20;
+            myAudio[0].Play();
+        }
+       
+    }
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
