734554b35f199211731a60a609ff83d49353cb9a
akim8085 <41929867+akim8085@users.noreply.github.com>
2020-12-06 03:07:14 -0500

    Add files via upload

---
+++ Code/MainMenuBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuBehavior : MonoBehaviour
+{
+   public void StartGameEasy()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void StartGameInter()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+    public void StartGameHard()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
